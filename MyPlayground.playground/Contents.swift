e4c6e77a2e76c337cad993a9bf19c3848bf72b82
danang swastiko <danang.sakti@sehatQ.com>
2019-11-06 19:05:16 +0700

    string counting with filter

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -28,3 +28,29 @@ for (index, item) in arr.reversed().enumerated() {
 for (index, item) in arr.reversed().enumerated() {
     print("Found \(item) at position \(index)")
 }
+
+
+
+let array = ["bola","bola","bolu","bolo","bola","bolu"]
+let unique = Array(Set(array))
+let filterz = array.filter{$0.contains("bola")}
+print(filterz.count)
+
+let arrUnique = Array(Set(array))
+   var arrCount = [Int]()
+   for (index, word) in arrUnique.enumerated() {
+	   let arrFiltered = array.filter {$0 == word}
+	   let intCount = arrFiltered.count
+	   arrCount.append(intCount)
+   }
+
+let totalMax = arrCount.max()
+var indexPopular = 0
+for  (index, total) in arrCount.enumerated() {
+	if total == totalMax {
+		indexPopular = index
+	}
+}
+
+print(arrUnique[indexPopular])
+print(arrCount)
